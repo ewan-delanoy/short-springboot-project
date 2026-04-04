37fe3fd7dee10f784f05ea3d6b7c928931969a1b
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-04 17:25:08 +0200

    add BinaryExpression

--- src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
@@ -22,7 +22,7 @@ import visible.org.apache.groovy.ast.tools.AnnotatedNodeUtils;
 import visible.org.apache.groovy.ast.tools.ClassNodeUtils;
 import visible.org.apache.groovy.lang.annotation.Incubating;
 import visible.org.codehaus.groovy.GroovyBugError;
-import org.codehaus.groovy.ast.expr.BinaryExpression;
+import visible.org.codehaus.groovy.ast.expr.BinaryExpression;
 import org.codehaus.groovy.ast.expr.Expression;
 import org.codehaus.groovy.ast.expr.FieldExpression;
 import org.codehaus.groovy.ast.expr.TupleExpression;
