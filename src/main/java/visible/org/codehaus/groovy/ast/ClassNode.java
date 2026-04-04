c7cd2533924e617ad53cc640a6adacd0b7180bc3
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-04 17:25:20 +0200

    add ParameterUtils

--- src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
@@ -29,7 +29,7 @@ import visible.org.codehaus.groovy.ast.expr.TupleExpression;
 import visible.org.codehaus.groovy.ast.stmt.BlockStatement;
 import visible.org.codehaus.groovy.ast.stmt.ExpressionStatement;
 import visible.org.codehaus.groovy.ast.stmt.Statement;
-import org.codehaus.groovy.ast.tools.ParameterUtils;
+import visible.org.codehaus.groovy.ast.tools.ParameterUtils;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.transform.ASTTransformation;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
