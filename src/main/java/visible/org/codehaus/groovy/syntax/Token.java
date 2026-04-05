976627d18cc11fd9625a1bd0f42ad651910c64c4
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 14:37:02 +0200

    add Reduction

--- src/main/java/visible/org/codehaus/groovy/syntax/Token.java
+++ src/main/java/visible/org/codehaus/groovy/syntax/Token.java
@@ -18,7 +18,7 @@
  */
 package visible.org.codehaus.groovy.syntax;
 
-import org.codehaus.groovy.GroovyBugError;
+import visible.org.codehaus.groovy.GroovyBugError;
 
 /**
  * A <code>CSTNode</code> produced by the <code>Lexer</code>.
