d757f0b9ae2946e5a6620a3c30920feb7a60ec44
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 11:39:23 +0200

    add VMPluginFactory

--- src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
@@ -33,7 +33,7 @@ import visible.org.codehaus.groovy.ast.tools.ParameterUtils;
 import visible.org.codehaus.groovy.control.CompilePhase;
 import visible.org.codehaus.groovy.transform.ASTTransformation;
 import visible.org.codehaus.groovy.transform.GroovyASTTransformation;
-import org.codehaus.groovy.vmplugin.VMPluginFactory;
+import visible.org.codehaus.groovy.vmplugin.VMPluginFactory;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
