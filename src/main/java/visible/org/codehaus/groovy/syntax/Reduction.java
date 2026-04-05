3166bd7779d563d26880916610430d79ca2fe55b
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 15:10:05 +0200

    fix some imports in StaticTypeCheckingSupport

--- src/main/java/visible/org/codehaus/groovy/syntax/Reduction.java
+++ src/main/java/visible/org/codehaus/groovy/syntax/Reduction.java
@@ -18,7 +18,7 @@
  */
 package visible.org.codehaus.groovy.syntax;
 
-import org.codehaus.groovy.GroovyBugError;
+import visible.org.codehaus.groovy.GroovyBugError;
 
 import java.util.ArrayList;
 import java.util.Collections;
