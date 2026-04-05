a8aab8698b98e5b6f516125fdbfef7eb13bc9934
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 12:24:22 +0200

    add FieldNode

--- src/main/java/visible/org/apache/groovy/ast/tools/ClassNodeUtils.java
+++ src/main/java/visible/org/apache/groovy/ast/tools/ClassNodeUtils.java
@@ -22,7 +22,7 @@ import visible.org.apache.groovy.util.BeanUtils;
 import visible.org.codehaus.groovy.ast.AnnotatedNode;
 import visible.org.codehaus.groovy.ast.ClassNode;
 import visible.org.codehaus.groovy.ast.ConstructorNode;
-import org.codehaus.groovy.ast.FieldNode;
+import visible.org.codehaus.groovy.ast.FieldNode;
 import org.codehaus.groovy.ast.MethodNode;
 import org.codehaus.groovy.ast.Parameter;
 import org.codehaus.groovy.ast.PropertyNode;
