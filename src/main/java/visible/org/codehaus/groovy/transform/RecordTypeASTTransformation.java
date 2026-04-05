796a17d3cce09bc8c9c11daf72750da9800952b5
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 12:21:25 +0200

    add ClassHelper

--- src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
+++ src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
@@ -72,7 +72,7 @@ import java.util.stream.Collectors;
 
 import static visible.org.apache.groovy.ast.tools.ClassNodeUtils.addGeneratedMethod;
 import static visible.org.codehaus.groovy.ast.ClassHelper.LIST_TYPE;
-import static org.codehaus.groovy.ast.ClassHelper.MAP_TYPE;
+import static visible.org.codehaus.groovy.ast.ClassHelper.MAP_TYPE;
 import static org.codehaus.groovy.ast.ClassHelper.OBJECT;
 import static org.codehaus.groovy.ast.ClassHelper.OBJECT_TYPE;
 import static org.codehaus.groovy.ast.ClassHelper.STRING_TYPE;
