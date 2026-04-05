80d6759d244d06f1269626dcf45041f1fc2aec32
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 11:48:28 +0200

    add Tuple14

--- src/main/java/visible/org/codehaus/groovy/ast/ClassHelper.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassHelper.java
@@ -35,7 +35,7 @@ import visible.groovy.lang.Tuple10;
 import visible.groovy.lang.Tuple11;
 import visible.groovy.lang.Tuple12;
 import visible.groovy.lang.Tuple13;
-import groovy.lang.Tuple14;
+import visible.groovy.lang.Tuple14;
 import groovy.lang.Tuple15;
 import groovy.lang.Tuple16;
 import groovy.lang.Tuple2;
