c240bb1be92e4104e0473ca95401fd0e7956b064
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 12:22:37 +0200

    add StringGroovyMethods

--- src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
+++ src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
@@ -113,7 +113,7 @@ import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.thisPropX;
 import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.throwS;
 import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.tryCatchS;
 import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.varX;
-import static org.codehaus.groovy.runtime.StringGroovyMethods.isAtLeast;
+import static visible.org.codehaus.groovy.runtime.StringGroovyMethods.isAtLeast;
 import static org.objectweb.asm.Opcodes.ACC_ABSTRACT;
 import static org.objectweb.asm.Opcodes.ACC_FINAL;
 import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
