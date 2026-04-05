a9f9f56e4a053ad468f1f90358e38c1afb2430dc
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 12:21:47 +0200

    add GeneralUtils

--- src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
+++ src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
@@ -85,7 +85,7 @@ import static visible.org.codehaus.groovy.ast.ClassHelper.make;
 import static visible.org.codehaus.groovy.ast.ClassHelper.makeWithoutCaching;
 import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.args;
 import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.arrayX;
-import static org.codehaus.groovy.ast.tools.GeneralUtils.assignS;
+import static visible.org.codehaus.groovy.ast.tools.GeneralUtils.assignS;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.bytecodeX;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.callThisX;
 import static org.codehaus.groovy.ast.tools.GeneralUtils.callX;
