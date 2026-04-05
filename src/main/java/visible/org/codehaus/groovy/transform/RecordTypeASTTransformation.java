974c3cee524b8c3376bf50f53eaf0f9934ca9b1e
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 12:22:45 +0200

    add Opcodes

--- src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
+++ src/main/java/visible/org/codehaus/groovy/transform/RecordTypeASTTransformation.java
@@ -118,7 +118,7 @@ import static visible.org.objectweb.asm.Opcodes.ACC_ABSTRACT;
 import static visible.org.objectweb.asm.Opcodes.ACC_FINAL;
 import static visible.org.objectweb.asm.Opcodes.ACC_PRIVATE;
 import static visible.org.objectweb.asm.Opcodes.ACC_PUBLIC;
-import static org.objectweb.asm.Opcodes.ACC_STATIC;
+import static visible.org.objectweb.asm.Opcodes.ACC_STATIC;
 import static org.objectweb.asm.Opcodes.ACC_SYNTHETIC;
 import static org.objectweb.asm.Opcodes.ALOAD;
 import static org.objectweb.asm.Opcodes.ARETURN;
