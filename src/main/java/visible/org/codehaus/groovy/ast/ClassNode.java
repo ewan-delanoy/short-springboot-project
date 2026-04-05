0e4c94ad79dfe4de9d8f9c01b7d492f3825d84fc
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 11:32:11 +0200

    add Traits

--- src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
@@ -55,7 +55,7 @@ import static java.util.stream.Collectors.toList;
 import static visible.org.apache.groovy.ast.tools.MethodNodeUtils.getCodeAsBlock;
 import static visible.org.codehaus.groovy.runtime.ArrayGroovyMethods.asBoolean;
 import static visible.org.codehaus.groovy.transform.RecordTypeASTTransformation.recordNative;
-import static org.codehaus.groovy.transform.trait.Traits.isTrait;
+import static visible.org.codehaus.groovy.transform.trait.Traits.isTrait;
 import static org.objectweb.asm.Opcodes.ACC_ABSTRACT;
 import static org.objectweb.asm.Opcodes.ACC_ANNOTATION;
 import static org.objectweb.asm.Opcodes.ACC_ENUM;
