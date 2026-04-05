267af2adf4d1b938afb4c82ee8e5ee60f217655d
ewan.delanoy <ewan.delanoy@zoho.com>
2026-04-05 11:39:44 +0200

    add MethodNodeUtils

--- src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
+++ src/main/java/visible/org/codehaus/groovy/ast/ClassNode.java
@@ -52,7 +52,7 @@ import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
-import static org.apache.groovy.ast.tools.MethodNodeUtils.getCodeAsBlock;
+import static visible.org.apache.groovy.ast.tools.MethodNodeUtils.getCodeAsBlock;
 import static org.codehaus.groovy.runtime.ArrayGroovyMethods.asBoolean;
 import static org.codehaus.groovy.transform.RecordTypeASTTransformation.recordNative;
 import static org.codehaus.groovy.transform.trait.Traits.isTrait;
